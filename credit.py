df8c2114997a0deab519953659aa50270cdfbe85
Nils Kappelmann <n.kappelmann@gmail.com>
2021-09-09 22:35:58 +0200

    added credit repayment attributes

--- credit.py
+++ credit.py
@@ -4,6 +4,10 @@ class Credit:
         self.interest = interest
         self.duration = duration
         self.annuity = self.calculate_annuity()
+        self.repayment_annual = round(self.annuity)
+        self.repayment_month = round(self.annuity / 12)
+        self.repayment_total = round(self.annuity * self.duration)
+        self.repayment_excess = self.repayment_total - self.size
 
     def calculate_annuity(self):
         annuity_factor = ((1+self.interest)**self.duration * self.interest)/((1+self.interest)**self.duration-1)
