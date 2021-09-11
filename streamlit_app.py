662c23d822651f57c318001d2e1dd9f5c20a186e
Nils Kappelmann <n.kappelmann@gmail.com>
2021-09-11 08:16:26 +0200

    testing script merging

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,30 @@
-# Main script
+# Credit calculation app
 
 import streamlit as st
-from credit import Credit
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import RendererAgg
 _lock = RendererAgg.lock
 
 st.title('Credit calculation app')
+
+# Define credtit class
+class Credit:
+    def __init__(self, size, interest=0.02, duration=30, currency='€'):
+        self.size = size
+        self.interest = interest
+        self.duration = duration
+        self.currency = currency
+        self.annuity = self.calculate_annuity()
+        self.repayment_annual = round(self.annuity)
+        self.repayment_month = round(self.annuity / 12)
+        self.repayment_total = round(self.annuity * self.duration)
+        self.repayment_excess = self.repayment_total - self.size
+
+    @st.cache
+    def calculate_annuity(self):
+        annuity_factor = ((1+self.interest)**self.duration * self.interest)/((1+self.interest)**self.duration-1)
+        annuity = self.size*annuity_factor
+        return annuity
 
 # Sidebar
 st.sidebar.title('Credit information')
