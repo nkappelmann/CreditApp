767a601eb77ec3ed165365c3a06054168765e038
Nils Kappelmann <n.kappelmann@gmail.com>
2021-10-03 11:45:54 +0200

    added second credit calculation and fixed interest rate functionality

--- credit.py
+++ credit.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 class Credit:
     def __init__(self, size, interest=0.02, duration=30, currency='€'):
         self.size = size
@@ -16,4 +19,24 @@ class Credit:
         return annuity
 
     def get_repayment_data(self):
-        pass
+        year = range(1, self.duration + 1)
+        interest_due = [self.size * self.interest]
+        repayment_due = [self.repayment_annual - interest_due[0]]
+        remaining_credit = [self.size - repayment_due[0]]
+
+        if self.duration > 1:
+            for i in range(2, self.duration + 1):
+                interest_due.append(remaining_credit[-1] * self.interest)
+                repayment_due.append(self.repayment_annual - interest_due[-1])
+                remaining_credit.append(remaining_credit[-1] - repayment_due[-1])
+
+        df = pd.DataFrame({
+            'Year': list(year),
+            'Interest': interest_due,
+            'repayment': repayment_due,
+            'Remaining credit': remaining_credit
+        })
+        return df
+
+        
+
